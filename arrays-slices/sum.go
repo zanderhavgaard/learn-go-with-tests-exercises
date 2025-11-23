97e36f12c6a4ee3d14b49fb5228b7060baa59efa
zanderhavgaard <zander@havgaard.dk>
2025-11-24 00:40:21 +0100

    finish arrays and slices

--- arrays-slices/sum.go
+++ arrays-slices/sum.go
@@ -7,3 +7,29 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		// technically retuns a new slice which includes the new sum
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
--- arrays-slices/sum_test.go
+++ arrays-slices/sum_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("Slice of arbitrary length", func(t *testing.T) {
@@ -13,5 +17,38 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d given %v", got, want, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	slice1 := []int{1, 2}
+	slice2 := []int{0, 9}
+	got := SumAll(slice1, slice2)
+	want := []int{3, 9}
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	compareSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("sum slices", func(t *testing.T) {
+		slice1 := []int{1, 2}
+		slice2 := []int{0, 9}
+		got := SumAllTails(slice1, slice2)
+		want := []int{2, 9}
+		compareSums(t, got, want)
+	})
+	t.Run("empty slice returns {0}", func(t *testing.T) {
+		slice1 := []int{}
+		slice2 := []int{0, 9}
+		got := SumAllTails(slice1, slice2)
+		want := []int{0, 9}
+		compareSums(t, got, want)
 	})
 }
