1d0cdafb8b4057e02c1e3311b6fcc10b4222f8d8
zanderhavgaard <zander@havgaard.dk>
2025-12-04 22:58:51 +0100

    mocking bonus

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"iter"
 	"os"
 	"time"
 )
@@ -31,8 +32,18 @@ func (c *ConfigurableSleeper) Sleep() {
 	c.sleep(c.duration)
 }
 
+func countDownFrom(from int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for i := from; i > 0; i-- {
+			if !yield(i) {
+				return
+			}
+		}
+	}
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
-	for i := countdownStart; i > 0; i-- {
+	for i := range countDownFrom(3) {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
