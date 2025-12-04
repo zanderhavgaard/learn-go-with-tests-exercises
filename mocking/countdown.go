fa54b3718df904c72259f6b2a2d9af0a12871b2c
zanderhavgaard <zander@havgaard.dk>
2025-12-04 22:56:56 +0100

    finish mocking

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -22,6 +22,15 @@ func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
@@ -31,6 +40,8 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeperDuration := 1 * time.Second
+	sleeperFunction := time.Sleep
+	sleeper := &ConfigurableSleeper{sleeperDuration, sleeperFunction}
 	Countdown(os.Stdout, sleeper)
 }
